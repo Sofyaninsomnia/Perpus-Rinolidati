e0a0be364c840d9984ccc067bbf2d76c572bab53
Sofyaninsomnia <ngodingsekarepe@gmail.com>
2025-08-30 23:15:08 +0700

    template admin & dashboard

---
+++ application/views/layouts/admin/footer.php
@@ -0,0 +1,24 @@
+ <footer>
+     <div class="footer clearfix mb-0 text-muted">
+         <div class="float-start">
+             <p>2022 &copy; Voler</p>
+         </div>
+         <div class="float-end">
+             <p>Crafted with <span class='text-danger'><i data-feather="heart"></i></span> by <a href="https://saugi.me">Saugi</a></p>
+         </div>
+     </div>
+ </footer>
+ </div>
+ </div>
+ <script src="<?= base_url(); ?>assets/admin/js/feather-icons/feather.min.js"></script>
+ <script src="<?= base_url(); ?>assets/admin/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+ <script src="<?= base_url(); ?>assets/admin/js/app.js"></script>
+
+ <script src="<?= base_url(); ?>assets/admin/vendors/chartjs/Chart.min.js"></script>
+ <script src="<?= base_url(); ?>assets/admin/vendors/apexcharts/apexcharts.min.js"></script>
+ <script src="<?= base_url(); ?>assets/admin/js/pages/dashboard.js"></script>
+
+ <script src="<?= base_url(); ?>assets/admin/js/main.js"></script>
+ </body>
+
+ </html>
---
+++ application/views/layouts/admin/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - Admin</title>
+    
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/admin/css/bootstrap.css">
+    
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/admin/vendors/chartjs/Chart.min.css">
+
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/admin/vendors/perfect-scrollbar/perfect-scrollbar.css">
+    <link rel="stylesheet" href="<?= base_url(); ?>assets/admin/css/app.css">
+    <link rel="shortcut icon" href="<?= base_url(); ?>assets/admin/images/favicon.svg" type="image/x-icon">
+</head>
